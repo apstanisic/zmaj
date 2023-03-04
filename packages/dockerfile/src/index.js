ab3f399aac7d3e6bc0f5d5ae4af1d37de180db86
Aleksandar Stanisic <aleksandar@tuta.io>
2023-03-04 22:22:53 +0100

    Add option to pass config to docker instance

--- packages/dockerfile/src/index.js
+++ packages/dockerfile/src/index.js
@@ -1,4 +1,20 @@
+import { readFile } from "fs/promises"
 import { runServer } from "zmaj"
 
+const config = await readFile("/app/zmaj-config.json", { encoding: "utf-8" })
+	.then((file) => {
+		try {
+			return JSON.parse(file)
+		} catch (error) {
+			// eslint-disable-next-line no-undef
+			console.error("Config is not a valid json file")
+		}
+	})
+	.catch((e) => {})
+
 // Allow user to mount .env file, don't disable reading .env
-await runServer({ migrations: { autoRunMigrations: true } })
+await runServer({
+	migrations: { autoRunMigrations: true },
+	config: { envPath: "/app/.env" },
+	...(config ?? {}),
+})
