4163ac0b0fb3f91bf881b4db9196b764845fe396
Aleksandar Stanisic <aleksandar@tuta.io>
2023-03-15 14:05:10 +0100

    Remove commented code

--- packages/email-templates/tsup.config.ts
+++ packages/email-templates/tsup.config.ts
@@ -1,28 +1,16 @@
-import { mkdir, rm, writeFile } from "fs/promises"
+import { writeFile } from "fs/promises"
 import { defineConfig } from "tsup"
 import { defaultTsupConfig } from "../../tsup.config"
 import { allTemplates } from "./src/jsx-templates/all"
 
 /**
- *
- * Generate templates, and put them in `dist/templates`
- * it's easier than to run 2 scripts, tsup and mjml -w
- * and for some reason mjml does not detects new files,
- * but tsup will detect when we integrate that file
- *
+ * This creates templates during build, that way MJML and React are dev dependencies.
+ * This should also reduce load a little, since templates are precompiled, and only
+ * variables are injected during runtime
  */
 async function compileEmails(): Promise<void> {
-	// const folderPath = "./src/jsx-templates"
-	// const files = await readdir(folderPath)
-
-	// sometimes this folder does not exist (on first run), so we ignore error
-	// await rm("./dist/templates", { recursive: true }).catch(() => {})
-	// await mkdir("./dist/templates")
-
 	console.log("Generating email templates")
-
 	await writeFile("./dist/templates.json", JSON.stringify(allTemplates, null, "\t"))
-
 	console.log(`Generated email templates`)
 }
 
