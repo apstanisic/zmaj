22fd15278a7140d2831f3138224aab553f2f9594
Aleksandar Stanisic <aleksandar@tuta.io>
2023-03-28 19:29:42 +0200

    Fix user stub type

--- packages/test-utils/src/stubs/user.stub.ts
+++ packages/test-utils/src/stubs/user.stub.ts
@@ -1,29 +1,29 @@
 import {
 	rand,
+	randAlphaNumeric,
 	randBoolean,
 	randEmail,
 	randFirstName,
 	randLastName,
 	randPastDate,
 } from "@ngneat/falso"
-import { ADMIN_ROLE_ID, PUBLIC_ROLE_ID, User, UserSchema, stub } from "@zmaj-js/common"
+import { ADMIN_ROLE_ID, PUBLIC_ROLE_ID, UserWithSecret, stub } from "@zmaj-js/common"
 import { v4 } from "uuid"
+import { chance } from "../chance.js"
 
 /**
  * This provides user stub with raw password
  */
 
-export const UserStub = stub<User>(
-	() => ({
-		id: v4(),
-		email: randEmail({ suffix: "test" }),
-		firstName: randFirstName(),
-		lastName: randLastName(),
-		status: rand(["active", "banned", "hacked", "disabled"]) as never,
-		roleId: rand([PUBLIC_ROLE_ID, ADMIN_ROLE_ID]),
-		confirmedEmail: randBoolean(),
-		createdAt: randPastDate({ years: 4 }),
-		password: "plain_password",
-	}),
-	UserSchema,
-)
+export const UserStub = stub<UserWithSecret>(() => ({
+	id: v4(),
+	email: randEmail({ suffix: "test" }),
+	firstName: randFirstName(),
+	lastName: randLastName(),
+	status: rand(["active", "banned", "hacked", "disabled"]) as never,
+	roleId: rand([PUBLIC_ROLE_ID, ADMIN_ROLE_ID]),
+	confirmedEmail: randBoolean(),
+	createdAt: randPastDate({ years: 4 }),
+	password: "plain_password",
+	otpToken: chance(0.2, () => randAlphaNumeric({ length: 40 }).join(""), null),
+}))
