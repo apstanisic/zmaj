1b108f7b77ef3780f5562c6e3c84dbe6cc5f228f
Aleksandar Stanisic <aleksandar@tuta.io>
2023-04-02 16:07:54 +0200

    fix ts error

--- packages/api/src/infra/infra.module-definition.ts
+++ packages/api/src/infra/infra.module-definition.ts
@@ -1,5 +1,5 @@
 import { ConfigurableModuleBuilder } from "@nestjs/common"
-import { InfraParams } from "./infra.config"
+import { InfraConfigParams } from "./infra.config"
 
 export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } =
-	new ConfigurableModuleBuilder<InfraParams>().build()
+	new ConfigurableModuleBuilder<InfraConfigParams>().build()
