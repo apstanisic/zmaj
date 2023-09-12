f3eb9c5e29bc188da3873eaa541186100cb6feda
Aleksandar Stanisic <aleksandar@tuta.io>
2023-09-12 23:51:08 +0200

    WIP

--- packages/client-sdk/src/clients/collections.client.ts
+++ packages/client-sdk/src/clients/collections.client.ts
@@ -1,8 +1,18 @@
 import { CollectionMetadataModel, endpoints } from "@zmaj-js/common"
+import { BaseModel } from "@zmaj-js/orm"
 import { AxiosInstance } from "axios"
 import { CrudClient } from "./crud.client"
 
-export class CollectionsClient extends CrudClient<CollectionMetadataModel> {
+class CollectionDefModel extends BaseModel {
+	override name = "collection_def"
+	fields = this.buildFields((f) => ({
+		...new CollectionMetadataModel().fields,
+		pkColumn: f.text({}),
+		pkType: f.enumString({ enum: ["auto-increment", "uuid"] }),
+	}))
+}
+
+export class CollectionsClient extends CrudClient<CollectionDefModel> {
 	constructor(http: AxiosInstance) {
 		super(http, endpoints.infraCollections.$base)
 	}
